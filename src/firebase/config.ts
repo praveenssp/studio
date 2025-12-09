d0f97bbe37ee91aaf7eec073e99e8eadede6e5d8
பிரவீன் தமிழ் நாடு <praveenssp81@gmail.com>
2025-12-09 10:28:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5394374359-8f03c",
+  "appId": "1:925918693986:web:20513ee292655cd21c5f3b",
+  "apiKey": "AIzaSyCADQY0gtfxNDXH3rvHaC0ovW-9SutspxQ",
+  "authDomain": "studio-5394374359-8f03c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "925918693986"
+};
